1c42fe877e8cf1918a5fcb61bfde778dc6414193
Igor Dzichkovskii <igordzich@gmail.com>
2020-02-26 11:26:28 +0100

    Putting the text of tv from coroutines. Works!

--- app/src/main/java/com/dzichkovskii/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/dzichkovskii/coroutinesexample/MainActivity.kt
@@ -2,20 +2,16 @@ package com.dzichkovskii.coroutinesexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import org.w3c.dom.Text
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
 
     private val RESULT_1 = "Result #1"
-    private var counter = 0
+    private var counter = 1
     private lateinit var tvCoroutines: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,17 +31,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setNewText(input: String){
+        val newText = tv_coroutines_pause_example.text.toString() + "\n$input"
+        tv_coroutines_pause_example.text = newText
+    }
+
+    private fun setNewText1(input: Int){
+        tv_coroutines_pause_example2.text = "Job #" + input.toString()
+    }
+
+    private suspend fun setTextOnMainThread(input: String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+
+    private suspend fun setTextOnMainThread1(input: Int){
+        withContext(Main){
+            setNewText1(input)
+        }
+    }
+
     private suspend fun fakeApiRequest(){
         val result1 = getResult1fromApi()
         println("debug: $result1")
+        setTextOnMainThread(result1)
     }
 
     private suspend fun getResult1fromApi():String {
         logThread("getResult1fromApi()")
         delay(1000)
-        // DOES NOT WORK, I need to google TODO(android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.)
-        //tvCoroutines.text = "Job #" + counter
+        setTextOnMainThread1(counter)
         counter++
+
         return RESULT_1
     }
 
